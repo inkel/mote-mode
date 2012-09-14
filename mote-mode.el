7e401c95e7604c894a760812fc04066ad333a10a
Leandro López (inkel) <inkel.ar@gmail.com>
2012-09-14 07:14:33 -0300

    Remove unused code

--- mote-mode.el
+++ mote-mode.el
@@ -10,33 +10,10 @@
 (defgroup mote nil
   "Mote mode customization group")
 
-(defgroup mote-faces nil
-  "Faces used in Mote minor mode."
-  :group 'mote
-  :group 'faces)
-
-(defface mote-comment-face '((t :inherit font-lock-comment-face))
-  "Face for Mote comments."
-  :group 'mote-faces)
-
-(defface mote-special-chars-face '((t :inherit font-lock-constant-face
-                                      :foreground "#eeaaaa"))
-  "Face for Mote's special characters %, {{ }}."
-  :group 'mote-faces)
-
-(defface mote-ruby-code-face '((t :inherit font-lock-reference-face
-                                  :weight bold))
-  "Face for Ruby code inside Mote files."
-  :group 'mote-faces)
-
-(defface mote-assignment-face '((t :inherit font-lock-variable-name-face))
-  "Face for assignments in Mote."
-  :group 'mote-faces)
-
 (defun mote-highlight-ruby (limit)
   "Highlight a Ruby expression."
-  (when (re-search-forward "^[ \t]*%\\(.*\\)$" limit t)
-    (mote-fontify-region-as-ruby (match-beginning 1) (match-end 1))))
+  (when (re-search-forward "^[ \t]*\\(%\\)\\(.*\\)$" limit t)
+    (mote-fontify-region-as-ruby (match-beginning 2) (match-end 2))))
 
 (defun mote-highlight-assignment (limit)
   "Highlight a Ruby expression inside an assignment."
@@ -55,10 +32,9 @@
         (font-lock-fontify-region beg end)))))
 
 (defvar mote-font-lock-keywords
-  '(("^[ \t]*%" 0 'mote-special-chars-face t)
-    ("\\({{\\).*\\(}}\\)"
-     (1 'mote-special-chars-face t)
-     (2 'mote-special-chars-face t))
+  '(("\\({{\\).*\\(}}\\)"
+     (1 'font-lock-preprocessor-face t)
+     (2 'font-lock-preprocessor-face t))
     (mote-highlight-assignment 1 font-lock-preprocessor-face)
     (mote-highlight-ruby 1 font-lock-preprocessor-face))
   "Additional syntax highlighting for Mote files.")
